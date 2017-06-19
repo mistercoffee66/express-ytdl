de7f09a450578c4594e61024c672ad2e9b48d2f1
sam.elwitt <sam.elwitt@ogilvy.com>
2017-06-18 23:05:44 -0400

    video wip

--- routes/convert.js
+++ routes/convert.js
@@ -14,15 +14,48 @@ const express = require('express'),
 
 router.get('/', (req, res) => {
 
-  convert(req.query.yturl, function(filename){
+  converty(req.query.yturl, function(filename){
+    console.log('done')
 
-    let mimetype = mime.lookup(filename);
-    res.set('Content-disposition', 'attachment; filename=' + filename);
-    res.set('Content-type', mimetype);
-    myReadableStreamBuffer.pipe(res);
-    myReadableStreamBuffer = new streamBuffers.ReadableStreamBuffer();
+    //let mimetype = mime.lookup(filename);
+    //res.set('Content-disposition', 'attachment; filename=' + filename);
+    //res.set('Content-type', mimetype);
+    //myReadableStreamBuffer.pipe(res);
+    //myReadableStreamBuffer = new streamBuffers.ReadableStreamBuffer();
   })
 });
+
+function converty(url,cb) {
+  let formats = [], max = 0, dlFormat, title
+  ytdl.getInfo(url, (err, info) => {
+    if (err) {
+      console.log('An error occurred: ' + process.cwd() + ' ' + err.message);
+    } else {
+      title = info.title
+      info.formats.map( function(format, i, arr)  {
+        if (format.container === 'mp4' && format.encoding === 'H.264') {
+          formats.push(format)
+        }
+      })
+      console.log('**********')
+      console.log(formats)
+      formats.map(function(format, i, arr){
+        let res = parseFloat(formats[i].resolution)
+        if (res > max) {
+          max = res
+          dlFormat = format
+        }
+      })
+      console.log(max)
+      ytdl(url, {format: dlFormat})
+        .pipe(fs.createWriteStream(title + '.mp4'))
+      cb()
+    }
+  })
+
+  //ytdl(url)
+  //  .pipe(fs.createWriteStream('video.mp4'))
+}
 
 function convert(url, cb) {
 
@@ -33,17 +66,18 @@ function convert(url, cb) {
       console.log('An error occurred: ' + process.cwd() + ' ' + err.message);
     }
     else {
-      filename = path.join(sanitize(info.title) + '.mp3');
+      filename = path.join(sanitize(info.title) + '.avi');
       ytStream = ytdl(url, {});
       ffStream = ffmpeg(ytStream)
         .on('error', (err) => {
           console.log('An error occurred: ' + process.cwd() + ' ' + err.message);
         })
-        .noVideo()
-        .audioChannels(2)
-        .audioBitrate('128k')
-        .audioCodec('libmp3lame')
-        .format('mp3')
+        //.noVideo()
+        //.audioChannels(2)
+        //.audioBitrate('128k')
+        //.audioCodec('libmp3lame')
+        //.format('mp4')
+        .preset('divx')
       ;
 
       ff = ffStream.pipe();
